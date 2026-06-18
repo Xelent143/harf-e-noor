1af0ef42f8dd8b6f8a5334a627aee8b86ec24173
Xelent143 <123277317+Xelent143@users.noreply.github.com>
2026-06-18 18:45:10 +0500

    Set sitemap content types

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const types = {
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
   ".png": "image/png",
   ".jpg": "image/jpeg",
   ".jpeg": "image/jpeg",
